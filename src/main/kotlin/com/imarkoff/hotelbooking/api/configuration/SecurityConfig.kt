3ffd9121f8bdd93e96b8f2b07a24c3e5f1c8707f
Vladyslav Hroshev <iv.markov02@gmail.com>
2025-10-26 19:35:33 +0200

    Enhance security configuration to require authentication for all requests

--- src/main/kotlin/com/imarkoff/hotelbooking/api/configuration/SecurityConfig.kt
+++ src/main/kotlin/com/imarkoff/hotelbooking/api/configuration/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.imarkoff.hotelbooking.api.configuration
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
@@ -14,6 +15,10 @@ class SecurityConfig {
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
             .csrf { it.disable() }
+            .authorizeHttpRequests {
+                it.anyRequest().authenticated()
+            }
+            .httpBasic (Customizer.withDefaults())
         return http.build()
     }
 }
